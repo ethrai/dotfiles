9683ce815339a06b519070deb1c652986f24cf31
Sergei Ryzhko <ethrai@proton.me>
2024-06-21 18:52:24 +0300

    add old config backup

---
+++ nvim_bak/lua/plugins/dapui.lua
@@ -0,0 +1,25 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		local dapui = require("dapui")
+    local dap = require("dap")
+		dapui.setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
+}
---
+++ nvim_bak/lua/vim-opts.lua
@@ -0,0 +1,19 @@
+vim.cmd("let mapleader=\" \"")
+
+vim.cmd("set nu")
+vim.cmd("set relativenumber")
+vim.cmd("set termguicolors")
+vim.cmd("set bg=dark")
+vim.cmd("set mouse=a")
+
+
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
+vim.cmd("set smartindent")
+
+vim.cmd("set noswapfile")
+vim.cmd("set nobackup")
+
+vim.cmd("set history=300")

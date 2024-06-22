76217aca8911905161d5c25d91a5ceb6c73b0264
Sergei Ryzhko <ethrai@proton.me>
2024-06-23 01:37:51 +0300

    use new variables and add more link

--- link.sh
+++ link.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/nvim ~/.config
-ln -s ~/dotfiles/tmux ~/.config
-ln -s ~/dotfiles/.ideavimrc ~
-ln -s ~/dotfiles/.gitconfig ~
-ln -s ~/dotfiles/.zshrc ~
-ln -s ~/dotfiles/alacritty ~/.config
-ln -s ~/dotfiles/ohmyposh/ ~/.config
+ln -s ${DFILES}/.ideavimrc ~
+ln -s ${DFILES}/.gitconfig ~
+ln -s ${DFILES}/.zshrc ~
+ln -s ${DFILES}/alacritty ${XDG_CONFIG_HOME}
+ln -s ${DFILES}/ohmyposh/ ${XDG_CONFIG_HOME}
+ln -s ${DFILES}/starship ${XDG_CONFIG_HOME}
+ln -s ${DFILES}/nvim ${XDG_CONFIG_HOME}
+ln -s ${DFILES}/tmux ${XDG_CONFIG_HOME}
+ln -s ${DFILES}/waybar ${XDG_CONFIG_HOME}
+ln -s ${DFILES}/hypr ${XDG_CONFIG_HOME}
 

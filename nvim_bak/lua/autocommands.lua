73a703bf8349cdd7c43103491b051d4549e7a0e4
Sergei Ryzhko <ethrai@proton.me>
2024-07-10 22:07:42 +0300

    remove useless stuff

--- nvim_bak/lua/autocommands.lua
+++
@@ -1,7 +0,0 @@
--- highlight yanked text
-vim.cmd([[
-augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
-augroup END
-]])

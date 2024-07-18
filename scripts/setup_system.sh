087a3b9715eaa3bfcaf4537d1942b287470760ea
Sergei Ryzhko <ethrai@proton.me>
2024-07-18 11:47:53 +0300

    update setup script

--- scripts/setup_system.sh
+++ scripts/setup_system.sh
@@ -5,18 +5,40 @@ function v() {
 	echo -e "\e[32m$@\e[0m"
 	"$@"
 }
-v sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+v sudo pacman -S --noconfirm --needed go git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 printf "Installing desktop packages..."
 
 v yay -S --noconfirm --needed \
 	hyprland polkit-kde-agent qt6-wayland \
 	hyprcursor hypridle hyprlang hyprlock hyprpaper hyprutils \
+	power-profiles-daemon playerctl pamixer brightnessctl pavucontrol \
 	xdg-desktop-portal-hyprland waybar dunst \
+	wlsunset wl-clipboard \
 	rofi-wayland rofi-emoji-git rofi-power-menu \
-	pipewire wireplumber \
-	blueman blueman bluez bluez-utils bluez-qt sddm \
-	intel-media-driver intel-ucode \
-	sof-firmware \
+	pipewire wireplumber plymouth blueman blueman bluez bluez-utils bluez-qt sddm sof-firmware intel-media-driver intel-ucode \
 	kitty tmux neovim vivaldi \
-	zoxide ripgrep fd bat zsh
+	qt6ct kvantum nwg-look \
+	zoxide ripgrep fzf treesitter fd bat zsh \
+	ttf-iosevka-term-ss04 ttf-nerd-fonts-symbols \
+	ttf-nerd-fonts-symbols-common ttf-font-awesome \
+	noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
+	terminus-font stow sudo opendoas \
+	reflector yt-dlp zip unzip nodejs \
+	telegram-desktop catppuccin-cursors-mocha \
+	catppuccin-gtk-theme-mocha kvantum-theme-catppuccin-git \
+	sddm-theme-catppuccin docker docker-compose git curl wget
+
+v sudo chsh -s "$(which zsh)" "$USER"
+
+v usermod -aG docker "$USER"
+v sudo systemctl enable docker.service
+v sudo systemctl enable docker.socket
+
+v sudo systemctl enable --now sddm
+v sudo systemctl enable --now NetworkManager
+
+printf "Installing desktop packages (done).\n"
+
+printf "Symlinking dotfiles..."
+v stow -v ~/dotfiles

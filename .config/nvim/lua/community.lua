0f2f3afd452dab430cea10102292d08ac40edc70
Sergei Ryzhko <ethrai@proton.me>
2024-07-21 02:02:28 +0300

    use theme from astrocommunity

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-
+  -- { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }

a5932f892b1cd8968c7e8ab310563e8dddd8def2
Sergei Ryzhko <ethrai@proton.me>
2024-07-29 22:39:26 +0300

    dont need it

--- install.sh
+++
@@ -1,9 +0,0 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-sudo pacman -S wget curl zsh vim alacritty ttf-jetbrains-mono-nerd zoxide fzf docker
-
-

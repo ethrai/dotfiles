7ebc435d7d476f2e1efeee2c4c602fe1ea3cb0a2
Sergei Ryzhko <ryzhkosergej@gmail.com>
2025-01-29 02:43:55 +0300

    add modus theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,15 @@ enable_transience
 function mark_prompt_start --on-event fish_prompt
     echo -en "\e]133;A\e\\"
 end
+
+source ~/.config/fish/modus-light.fish
+
+
+set -Ux FZF_DEFAULT_OPTS "\
+  --color=fg:#000000,fg+:#193668,bg:#ffffff,bg+:#f0f0f0 \
+  --color=hl:#005e8b,hl+:#008899,info:#afaf87,marker:#00663f \
+  --color=prompt:#a0132f,spinner:#7b435c,pointer:#7b435c,header:#87afaf \
+  --color=border:#9f9f9f,label:#aeaeae,query:#595959 \
+  --border="sharp" --border-label="fzf" --border-label-pos="0" --preview-window="border-sharp" \
+  --padding="1" --prompt="> " --marker=">" --pointer="◆" \
+  --separator="─" --scrollbar="" --layout="reverse" --info="right""

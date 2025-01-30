f6d8f10d4b8efd81e88555cc046b89235e179fbe
Sergei Ryzhko <ryzhkosergej@gmail.com>
2025-01-30 20:59:29 +0300

    fix(nvim): lazyload treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,72 +2,17 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = "BufEnter",
     config = function()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "go",
-          "javascript",
-          "html",
-          "templ",
-          "python",
-          "bash",
-        },
+        ensure_installed = {},
         auto_install = true,
         ignore_install = {},
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
-        textobjects = {
-          enable = false,
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["ai"] = "@conditional.outer",
-              ["ii"] = "@conditional.inner",
-              ["il"] = "@loop.inner",
-              ["al"] = "@loop.outer",
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-            },
-          },
-        },
         modules = {},
       })
     end,
@@ -75,19 +20,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-context",
     opts = {
-      enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-      multiwindow = false,      -- Enable multiwindow support.
-      max_lines = 3,            -- How many lines the window should span. Values <= 0 mean no limit.
-      min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-      line_numbers = true,
-      multiline_threshold = 20, -- Maximum number of lines to show for a single context
-      trim_scope = "outer",     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-      mode = "cursor",          -- Line used to calculate context. Choices: 'cursor', 'topline'
-      -- Separator between context and content. Should be a single character string, like '-'.
-      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-      separator = nil,
-      zindex = 20, -- The Z-index of the context window
-      on_attach = nil,
+      enable = true,      -- Enable this plugin (Can be enabled/disabled later via commands)
+      multiwindow = true, -- Enable multiwindow support.
+      max_lines = 3,      -- How many lines the window should span. Values <= 0 mean no limit.
+      mode = "cursor",    -- Line used to calculate context. Choices: 'cursor', 'topline'
     },
   },
 }
